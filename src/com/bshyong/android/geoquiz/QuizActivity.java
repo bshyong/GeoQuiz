97c97d619690930cf07c6f54d833dc5c7c030023
Benjamin Shyong <ben.shyong@gmail.com>
2014-01-04 14:48:22 -0800

    add question array

--- src/com/bshyong/android/geoquiz/QuizActivity.java
+++ src/com/bshyong/android/geoquiz/QuizActivity.java
@@ -5,18 +5,36 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 	
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+		new TrueFalse(R.string.question_oceans, true),
+		new TrueFalse(R.string.question_mideast, false),
+		new TrueFalse(R.string.question_africa, false),
+		new TrueFalse(R.string.question_americas, true),
+		new TrueFalse(R.string.question_asia, true)
+	};
+	private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // inflate ActivityQuiz layout
         setContentView(R.layout.activity_quiz);
+        
+        // get reference for TextView and set text to current question at index
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        // reference ids are integers
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
         
         // get and assign references to widgets
         mTrueButton = (Button)findViewById(R.id.true_button);
