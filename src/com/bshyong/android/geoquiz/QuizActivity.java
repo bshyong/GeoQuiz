b39559a614fea96898c61a8f9d672b5e60de5732
Benjamin Shyong <ben.shyong@gmail.com>
2014-01-04 01:44:01 -0800

    link buttons and add click listeners

--- src/com/bshyong/android/geoquiz/QuizActivity.java
+++ src/com/bshyong/android/geoquiz/QuizActivity.java
@@ -1,16 +1,43 @@
 package com.bshyong.android.geoquiz;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+	
+	private Button mTrueButton;
+	private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // inflate ActivityQuiz layout
         setContentView(R.layout.activity_quiz);
+        
+        // get and assign references to widgets
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			  Toast.makeText(QuizActivity.this, 
+					  		 R.string.incorrect_toast, 
+					  		 Toast.LENGTH_SHORT).show();
+			}
+		});
+
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			  Toast.makeText(QuizActivity.this, 
+				  		 R.string.correct_toast, 
+				  		 Toast.LENGTH_SHORT).show();
+			}
+		});
     }
 
     @Override
